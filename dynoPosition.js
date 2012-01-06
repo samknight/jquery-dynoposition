edfa24afe87983a8acfddb6d635c793478de3c1c
Sam Knight <sam@samknight.co.uk>
2012-01-06 10:14:00 +0000

    Use soft tabs

--- dynoPosition.js
+++ dynoPosition.js
@@ -1,29 +1,29 @@
 (function( $ ){
   $.fn.dynoPosition = function(options) {
-		var defaults = { 
-		    dynamicDiv: "#" + this.attr("id"),
-				compareDiv: "#sidebar",
-				betweenElement: "div"
-		};
+    var defaults = { 
+      dynamicDiv: "#" + this.attr("id"),
+      compareDiv: "#sidebar",
+      betweenElement: "div"
+    };
 
-		var options = $.extend({}, defaults, options);
-		inserted_ad = 0;
-		$(options.dynamicDiv).hide();
-		guide = $(options.compareDiv).offset().top + $(options.compareDiv).height();
-		$(options.betweenElement).each(function(index){
-			bottom = $(this).offset().top + $(this).height();
-			if(guide <= bottom && inserted_ad == 0) {
-				$(options.dynamicDiv).show();
-				dynamicDivCode = $(options.dynamicDiv).clone();
-				$(options.dynamicDiv).hide();
-				$(this).after(dynamicDivCode);
-				inserted_ad = 1;
-			}
-		});
-		
-		if(inserted_ad == 0) {
-			$(options.dynamicDiv).show();
-		}
+    var options = $.extend({}, defaults, options);
+    inserted_ad = 0;
+    $(options.dynamicDiv).hide();
+    guide = $(options.compareDiv).offset().top + $(options.compareDiv).height();
+    $(options.betweenElement).each(function(index){
+      bottom = $(this).offset().top + $(this).height();
+      if(guide <= bottom && inserted_ad == 0) {
+        $(options.dynamicDiv).show();
+        dynamicDivCode = $(options.dynamicDiv).clone();
+        $(options.dynamicDiv).hide();
+        $(this).after(dynamicDivCode);
+        inserted_ad = 1;
+      }
+    });
+
+    if(inserted_ad == 0) {
+    $(options.dynamicDiv).show();
+    }
   };	
 })( jQuery );
 
